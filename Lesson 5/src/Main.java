9bd779a6f913767d9dbed7fc64413e0fe361a31c
Maxpower1002 <148002355+Maxpower1002@users.noreply.github.com>
2023-10-24 19:50:44 +0300

    lesson 1

---
+++ Lesson 5/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      int a=new Scanner(System.in).nextInt();
+      if ( a>35){
+          System.out.println("Hello world");
+      }
+      if (a<5){
+          System.out.println("Goodbye world");
+      }
+    }
+}
