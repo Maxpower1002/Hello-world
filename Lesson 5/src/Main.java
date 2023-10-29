e66b1c5b1a706c5fdc4f9ada39e2c9915d16cccf
Maxpower1002 <148002355+Maxpower1002@users.noreply.github.com>
2023-10-29 10:50:22 +0200

    lesson 6

--- Lesson 5/src/Main.java
+++ Lesson 5/src/Main.java
@@ -2,12 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      int a=new Scanner(System.in).nextInt();
-      if ( a>35){
-          System.out.println("Hello world");
-      }
-      if (a<5){
-          System.out.println("Goodbye world");
-      }
+       int twixPrice =5;
+       int marsPrice =5;
+       int snickersPrice =6;
+       int colaPrice =7;
+       boolean isCanBuy=false;
+
+       System.out.println("Enter the amount of money");
+       int moneyAmount =new Scanner(System.in).nextInt();
+       if (moneyAmount>= twixPrice) {
+           System.out.println("You can buy twix");
+           isCanBuy=true;
+        }
+       if (moneyAmount>=marsPrice) {
+           System.out.println("You can buy mars");
+           isCanBuy=true;
+       }
+        if (moneyAmount>=snickersPrice)  {
+            System.out.println("You can buy snickers");
+            isCanBuy=true;
+        }
+
+       if (moneyAmount>=colaPrice) {
+           System.out.println("You can buy cola");
+           isCanBuy=true;
+       }
+       if (isCanBuy==false)
+           System.out.println("Fuck you.No money no funny");
+
     }
 }
